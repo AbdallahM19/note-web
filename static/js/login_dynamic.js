ba33d8558bba28f977d29d819ac002e77a6f908c
AbdallahM19 <abdullahmaabakar@gmail.com>
2024-12-18 14:12:19 +0200

    Refactor login dynamic JavaScript file and add dark mode functionality

--- static/js/login_dynamic.js
+++ static/js/login_dynamic.js
@@ -1,30 +1,33 @@
+import { darkModeDy } from "./mode_color.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-    const signin_btn=document.querySelector("#signin_btn");
-    const signup_btn=document.querySelector("#signup_btn");
-    const signin_btn2=document.querySelector("#signin_btn2");
-    const signup_btn2=document.querySelector("#signup_btn2");
-    const container=document.querySelector(".container");
+  const signin_btn = document.querySelector("#signin_btn");
+  const signup_btn = document.querySelector("#signup_btn");
+  const signin_btn2 = document.querySelector("#signin_btn2");
+  const signup_btn2 = document.querySelector("#signup_btn2");
+  const container = document.querySelector(".container");
 
-    signup_btn.addEventListener("click", () =>{
-        container.classList.add("sign-up-mode");
-        container.classList.add("signup-mode2");
-    });
+  signup_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+    container.classList.add("signup-mode2");
+  });
 
-    signin_btn.addEventListener("click", () =>{
-        container.classList.remove("sign-up-mode");
-        container.classList.remove("signup-mode2");
-    });
+  signin_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+    container.classList.remove("signup-mode2");
+  });
 
-    signup_btn2.addEventListener("click", (e) => {
-        e.preventDefault();
-        container.classList.add("sign-up-mode");
-        container.classList.add("signup-mode2");
-    });
+  signup_btn2.addEventListener("click", (e) => {
+    e.preventDefault();
+    container.classList.add("sign-up-mode");
+    container.classList.add("signup-mode2");
+  });
 
-    signin_btn2.addEventListener("click", (e) => {
-        e.preventDefault();
-        container.classList.remove("signup-mode2");
-        container.classList.remove("sign-up-mode");
-    });
+  signin_btn2.addEventListener("click", (e) => {
+    e.preventDefault();
+    container.classList.remove("signup-mode2");
+    container.classList.remove("sign-up-mode");
+  });
+
+  darkModeDy();
 });
-    
--- static/js/mode_color.js
+++ static/js/mode_color.js
@@ -1,6 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+export function darkModeDy() {
   const darkModeToggle = document.getElementById("darkModeToggle");
   const modeIcon = document.getElementById("modeIcon");
+
+  const savedMode = localStorage.getItem("theme");
+  if (savedMode === "dark") {
+    document.body.classList.add("dark-mode");
+    modeIcon.src = "../static/images/sun-icon.png";
+    modeIcon.alt = "Light Mode";
+  } else {
+    document.body.classList.remove("dark-mode");
+    modeIcon.src = "../static/images/dark-mode.png";
+    modeIcon.alt = "Dark Mode";
+  }
 
   darkModeToggle.addEventListener("click", () => {
     const isDarkMode = document.body.classList.contains("dark-mode");
@@ -11,5 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ? "../static/images/dark-mode.png"
       : "../static/images/sun-icon.png";
     modeIcon.alt = isDarkMode ? "Light Mode" : "Dark Mode";
+
+    localStorage.setItem("theme", isDarkMode ? "light" : "dark");
   });
-});
+}
