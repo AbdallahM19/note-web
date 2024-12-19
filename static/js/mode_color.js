d67199c11728373ffb307d2bedca582e795767d8
AbdallahM19 <abdullahmaabakar@gmail.com>
2024-12-19 10:57:37 +0200

    Added dark mode functionality to login page and updated image sources in mode_color.js

--- static/js/mode_color.js
+++ static/js/mode_color.js
@@ -2,6 +2,19 @@ export function darkModeDy() {
   const darkModeToggle = document.getElementById("darkModeToggle");
   const modeIcon = document.getElementById("modeIcon");
 
+  const panels_container = document.querySelector(".panels-container");
+  const left_panel = panels_container.querySelector(".left-panel");
+  const left_img_in_panel = left_panel.querySelector(".images");
+  const right_panel = panels_container.querySelector(".right-panel");
+  const right_img_in_panel = right_panel.querySelector(".images");
+
+  console.log('-------------------------');
+  console.log(container);
+  console.log(panels_container);
+  console.log(left_panel);
+  console.log(right_panel);
+  console.log('-------------------------');
+
   const savedMode = localStorage.getItem("theme");
   if (savedMode === "dark") {
     document.body.classList.add("dark-mode");
@@ -18,10 +31,16 @@ export function darkModeDy() {
 
     document.body.classList.toggle("dark-mode");
 
+    modeIcon.alt = isDarkMode ? "Light Mode" : "Dark Mode";
     modeIcon.src = isDarkMode
       ? "../static/images/dark-mode.png"
       : "../static/images/sun-icon.png";
-    modeIcon.alt = isDarkMode ? "Light Mode" : "Dark Mode";
+    left_img_in_panel.src = isDarkMode
+      ? "../static/images/social-media.gif"
+      : "../static/images/sign-in.jpeg";
+    right_img_in_panel.src = isDarkMode
+      ? "../static/images/social-media.gif"
+      : "../static/images/sign-up.jpeg";
 
     localStorage.setItem("theme", isDarkMode ? "light" : "dark");
   });
