85b646c67b43d69efe89b62316396e1a92683b75
AbdallahM19 <abdullahmaabakar@gmail.com>
2024-12-21 15:41:59 +0200

    Added HTTPException for missing request object in load_current_user decorator

--- api/settings.py
+++ api/settings.py
@@ -1,4 +1,4 @@
-from fastapi import Request
+from fastapi import Request, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from api.utils.session import SessionManager, get_session_manager
 from functools import wraps
@@ -6,7 +6,14 @@ from functools import wraps
 
 def load_current_user(func):
     @wraps(func)
-    async def wrapper(req: Request, *args, **kwargs):
+    async def wrapper(*args, **kwargs):
+        req = kwargs.get("req") or (args[0] if args and isinstance(args[0], Request) else None)
+        if not req:
+            raise HTTPException(
+                status_code=401,
+                detail="Missing request object",
+            )
+
         session_manager: SessionManager = await get_session_manager(req)
 
         if session_manager.user_id or session_manager.session_id:
@@ -14,5 +21,5 @@ def load_current_user(func):
                 url="/home",
                 status_code=302,
             )
-        return await func(req, *args, **kwargs)
+        return await func(*args, **kwargs)
     return wrapper
