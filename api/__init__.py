ac143b74220b41344004e4637eb4aebf5821eec8
AbdallahM19 <abdullahmaabakar@gmail.com>
2025-01-12 16:51:30 +0200

    add exception_handler to return error page include error details

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,9 @@
 """__init__.py"""
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
-from api.app import router
+from api.app import router, templates
 from api.routers.user_api import router as user_router
 from api.routers.note_api import router as note_router
 from api.database import create_database, create_tables, drop_db
@@ -35,6 +35,34 @@ async def before_close_app():
     # drop_db()
     print("Application shutdown complete")
 
+
+# Error Handler
+@app.exception_handler(HTTPException)
+async def not_found_exception_handler(req: Request, exc: HTTPException):
+    """called when an HTTPException is raised"""
+    return templates.TemplateResponse(
+        request=req,
+        name="error_page.html",
+        context={
+            "error_code": exc.status_code,
+            "error_message": exc.detail or "Something went wrong",
+            "error_detail": "Please contact support if the issue persists."
+        },
+        status_code=exc.status_code
+    )
+
+# @app.exception_handler(500)
+# async def internal_server_error_handler(req: Request, exc: HTTPException):
+#     """Custom 500 error handler"""
+#     return templates.TemplateResponse(
+#         request=req,
+#         name="error_page.html",
+#         context={
+#             "error_code": exc.status_code,
+#             "error_message": "Internal Server Error",
+#             "error_detail": exc.detail
+#         }
+#     )
 
 # def logger(func):
 #     """logger function"""
